4cbc1a1bc06240d85f3b062fc228004ad99bd5f7
basakest <ab1652759879@gmail.com>
2021-04-09 21:48:25 +0800

    test18

--- src/Adapters/DatabaseAdapter.php
+++ src/Adapters/DatabaseAdapter.php
@@ -208,7 +208,7 @@ class DatabaseAdapter implements Adapter, BatchAdapter
         }
         $keys = array_unique($keys);
         foreach ($keys as $key) {
-            $instance->find($key, $con[$key]);
+            $instance->whereIn($key, $con[$key]);
         }
         $instance->delete();
     }
